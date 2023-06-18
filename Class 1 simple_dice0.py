413db713f3d0009f046b5116374470e8e350fc20
luxihouse <90531412+luxihouse@users.noreply.github.com>
2023-06-18 09:36:41 -0500

    Add files via upload
    Add c;ass 1 fo;es

---
+++ Class 1 simple_dice0.py
@@ -0,0 +1,23 @@
+##
+## simple_dice.py
+
+import random
+
+def roll(sides=6):
+    num_rolled = random.randint(1,sides)
+    return num_rolled
+
+def main():
+    sides = 6
+    rolling = True
+    while rolling:
+    	roll_again = input("Ready to roll? ENTER=Roll. Q=Quite. ")
+        if roll_again.lower() != "q":
+    	    num_rolled = roll(sides)
+            print("You rolled a ", num_rolled)
+        else:
+            rolling = False
+
+    print("Thanks for playing.")
+
+main()
