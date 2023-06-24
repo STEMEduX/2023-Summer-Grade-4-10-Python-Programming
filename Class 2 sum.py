062975eecc484a408e6ff497e9f3910a685458c4
luxihouse <90531412+luxihouse@users.noreply.github.com>
2023-06-24 18:47:38 -0500

    Add files via upload
    Add sum.py.

---
+++ Class 2 sum.py
@@ -0,0 +1,16 @@
+#write a python program to calculate 
+#1 + 2 + 3 + ... + 99 + 100.
+
+x = 0
+total = 0
+while 1 == 1:
+    x = x + 1
+    #if x < 50:
+    #    continue
+    if x > 100:
+        break
+    total = total + x
+    print(x)
+
+
+print(total)
