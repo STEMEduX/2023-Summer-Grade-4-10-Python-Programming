481524d38491542363465855c450dc8cbe9f91b8
luxihouse <90531412+luxihouse@users.noreply.github.com>
2023-07-23 20:33:44 -0500

    Soutions Class 5 quiz.py

--- Class 5 quiz prob 2.py
+++ Class 5 quiz prob 2.py
@@ -1,3 +1,12 @@
+## Quiz Problem 1
+rows = eval(input('Please input how many rows: '))
+cols = eval(input('Please input how many cols: '))
+
+print('')
+for i in range(rows):
+    print('*'* cols)
+
+# Quiz Problem 2
 rows = eval(input('Please input how many rows: '))
 cols = eval(input('Please input how many cols: '))
 
@@ -7,3 +16,7 @@ for i in range(rows):
         print('*'* cols)
     else:
         print('*' + (' ') * (cols - 2) + '*')
+
+# Quiz Problem 3
+for i in range(100, 0, -2):
+    print(i, end=' ')
