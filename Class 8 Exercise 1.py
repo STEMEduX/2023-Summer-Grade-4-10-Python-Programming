5dacdbcbaadf35e671bf649a09bcb019119bbe22
luxihouse <90531412+luxihouse@users.noreply.github.com>
2023-08-12 14:47:00 -0500

    Create Class 8 Exercise 1.py

---
+++ Class 8 Exercise 1.py
@@ -0,0 +1,8 @@
+
+for i in range(0,20):
+    if i/2 != 0:
+#        print(' '* 20 + '*'*i)
+        print(' '* (20 - i) + '*' * (i - 1)  + '*'*i)
+
+for i in range (0, 10):
+    print(' '*17 + '|||||')
